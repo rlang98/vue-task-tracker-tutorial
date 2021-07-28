38bbc3009919dd3cbf3b419b110ee3d73fc45128
Riley Langrehr <rileylangrehr@gmail.com>
2021-07-28 21:40:34 +0930

    Initial commit to repository

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//Entry point for Vue
+//create app, pasing in App from vue, mounting it to the div with the Id of app in index.html
 createApp(App).mount('#app')
